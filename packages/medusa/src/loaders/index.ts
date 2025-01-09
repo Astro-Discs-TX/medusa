cebeaba57f0916d96284f9b83857a222a0f86c36
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2025-01-09 18:23:21 +0100

    discover extensions from plugins

--- packages/medusa/src/loaders/index.ts
+++ packages/medusa/src/loaders/index.ts
@@ -1,3 +1,12 @@
+import { container, MedusaAppLoader } from "@medusajs/framework"
+import { configLoader } from "@medusajs/framework/config"
+import { pgConnectionLoader } from "@medusajs/framework/database"
+import { featureFlagsLoader } from "@medusajs/framework/feature-flags"
+import { expressLoader } from "@medusajs/framework/http"
+import { JobLoader } from "@medusajs/framework/jobs"
+import { LinkLoader } from "@medusajs/framework/links"
+import { logger } from "@medusajs/framework/logger"
+import { SubscriberLoader } from "@medusajs/framework/subscribers"
 import {
   ConfigModule,
   LoadedModule,
@@ -9,6 +18,7 @@ import {
   GraphQLSchema,
   promiseAll,
 } from "@medusajs/framework/utils"
+import { WorkflowLoader } from "@medusajs/framework/workflows"
 import { asValue } from "awilix"
 import { Express, NextFunction, Request, Response } from "express"
 import { join } from "path"
@@ -16,16 +26,6 @@ import requestIp from "request-ip"
 import { v4 } from "uuid"
 import adminLoader from "./admin"
 import apiLoader from "./api"
-import { configLoader } from "@medusajs/framework/config"
-import { expressLoader } from "@medusajs/framework/http"
-import { JobLoader } from "@medusajs/framework/jobs"
-import { LinkLoader } from "@medusajs/framework/links"
-import { logger } from "@medusajs/framework/logger"
-import { container, MedusaAppLoader } from "@medusajs/framework"
-import { pgConnectionLoader } from "@medusajs/framework/database"
-import { SubscriberLoader } from "@medusajs/framework/subscribers"
-import { WorkflowLoader } from "@medusajs/framework/workflows"
-import { featureFlagsLoader } from "@medusajs/framework/feature-flags"
 import { getResolvedPlugins } from "./helpers/resolve-plugins"
 
 type Options = {
@@ -106,7 +106,7 @@ async function loadEntrypoints(
     next()
   })
 
-  await adminLoader({ app: expressApp, configModule, rootDirectory })
+  await adminLoader({ app: expressApp, configModule, rootDirectory, plugins })
   await apiLoader({
     container,
     plugins,
