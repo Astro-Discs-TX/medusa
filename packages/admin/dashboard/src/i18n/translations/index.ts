dc286f671d692c8cfddf7f582ae14815a82c0828
Kermit <k84101311@gmail.com>
2025-01-09 20:58:13 +0800

    feat: add translation for zh-tw
    This PR adds the zh-tw translation for Medusa Admin.
    
    All keys in zh-tw.json have been translated.
    The file has been validated locally using yarn i18n:validate, and no issues were found.

--- packages/admin/dashboard/src/i18n/translations/index.ts
+++ packages/admin/dashboard/src/i18n/translations/index.ts
@@ -9,6 +9,7 @@ import ptBR from "./ptBR.json"
 import th from "./th.json"
 import tr from "./tr.json"
 import uk from "./uk.json"
+import zhTw from "./zh-tw.json"
 
 export default {
   de: {
@@ -43,5 +44,8 @@ export default {
   },
   uk: {
     translation: uk,
+  },
+  zhTw: {
+    translation: zhTw,
   },
 }
