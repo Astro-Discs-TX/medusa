2c46c2668d03688acb0894b563247cf65226d287
Attachai77 <attachai@zimpligital.com>
2025-01-15 19:10:08 +0700

    feat: add promotion created event

--- packages/core/core-flows/src/promotion/workflows/create-promotions.ts
+++ packages/core/core-flows/src/promotion/workflows/create-promotions.ts
@@ -1,30 +1,35 @@
-import { AdditionalData, CreatePromotionDTO } from "@medusajs/framework/types"
+import type {
+	AdditionalData,
+	CreatePromotionDTO,
+} from "@medusajs/framework/types";
 import {
-  WorkflowResponse,
-  createHook,
-  createWorkflow,
-} from "@medusajs/framework/workflows-sdk"
-import { createPromotionsStep } from "../steps"
+	WorkflowResponse,
+	createHook,
+	createWorkflow,
+	transform,
+} from "@medusajs/framework/workflows-sdk";
+import { createPromotionsStep } from "../steps";
+import { emitEventStep } from "../../common";
 
 /**
  * The data to create one or more promotions, along with custom data that's passed to the workflow's hooks.
  */
 export type CreatePromotionsWorkflowInput = {
-  /**
-   * The promotions to create.
-   */
-  promotionsData: CreatePromotionDTO[]
-} & AdditionalData
+	/**
+	 * The promotions to create.
+	 */
+	promotionsData: CreatePromotionDTO[];
+} & AdditionalData;
 
-export const createPromotionsWorkflowId = "create-promotions"
+export const createPromotionsWorkflowId = "create-promotions";
 /**
  * This workflow creates one or more promotions. It's used by the [Create Promotion Admin API Route](https://docs.medusajs.com/api/admin#promotions_postpromotions).
- * 
+ *
  * This workflow has a hook that allows you to perform custom actions on the created promotion. For example, you can pass under `additional_data` custom data that
  * allows you to create custom data models linked to the promotions.
- * 
+ *
  * You can also use this workflow within your own custom workflows, allowing you to wrap custom logic around creating promotions.
- * 
+ *
  * @example
  * const { result } = await createPromotionsWorkflow(container)
  * .run({
@@ -47,24 +52,39 @@ export const createPromotionsWorkflowId = "create-promotions"
  *     }
  *   }
  * })
- * 
+ *
  * @summary
- * 
+ *
  * Create one or more promotions.
- * 
+ *
  * @property hooks.promotionsCreated - This hook is executed after the promotions are created. You can consume this hook to perform custom actions on the created promotions.
  */
 export const createPromotionsWorkflow = createWorkflow(
-  createPromotionsWorkflowId,
-  (input: CreatePromotionsWorkflowInput) => {
-    const createdPromotions = createPromotionsStep(input.promotionsData)
-    const promotionsCreated = createHook("promotionsCreated", {
-      promotions: createdPromotions,
-      additional_data: input.additional_data,
-    })
+	createPromotionsWorkflowId,
+	(input: CreatePromotionsWorkflowInput) => {
+		const createdPromotions = createPromotionsStep(input.promotionsData);
 
-    return new WorkflowResponse(createdPromotions, {
-      hooks: [promotionsCreated],
-    })
-  }
-)
+		const promotionIdEvents = transform(
+			{ createdPromotions },
+			({ createdPromotions }) => {
+				return createdPromotions.map((v) => {
+					return { id: v.id };
+				});
+			},
+		);
+
+		emitEventStep({
+			eventName: "promotion_created",
+			data: promotionIdEvents,
+		});
+
+		const promotionsCreated = createHook("promotionsCreated", {
+			promotions: createdPromotions,
+			additional_data: input.additional_data,
+		});
+
+		return new WorkflowResponse(createdPromotions, {
+			hooks: [promotionsCreated],
+		});
+	},
+);
