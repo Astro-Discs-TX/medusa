65830343d6f6285d08319721239e1ec037878bfb
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2025-01-23 09:25:38 -0300

    conflict

--- packages/modules/fulfillment/src/migrations/Migration20250113122235.ts
+++ packages/modules/fulfillment/src/migrations/Migration20250113122235.ts
@@ -4,20 +4,12 @@ export class Migration20250113122235 extends Migration {
   override async up(): Promise<void> {
     this.addSql(`
       UPDATE shipping_option_rule
-<<<<<<< HEAD
       SET value = '"true"'
-=======
-      SET value = 'true'::jsonb
->>>>>>> 5aa47978734b4deae5139a4fd5eba15fd20cb874
       WHERE value = '"\\"true\\""';
     `)
     this.addSql(`
       UPDATE shipping_option_rule
-<<<<<<< HEAD
       SET value = '"false"'
-=======
-      SET value = 'false'::jsonb
->>>>>>> 5aa47978734b4deae5139a4fd5eba15fd20cb874
       WHERE value = '"\\"false\\""';
     `)
   }
@@ -26,20 +18,12 @@ export class Migration20250113122235 extends Migration {
     this.addSql(`
       UPDATE shipping_option_rule
       SET value = '"\\"true\\""'
-<<<<<<< HEAD
       WHERE value = '"true"';
-=======
-      WHERE value = 'true'::jsonb;
->>>>>>> 5aa47978734b4deae5139a4fd5eba15fd20cb874
     `)
     this.addSql(`
       UPDATE shipping_option_rule
       SET value = '"\\"false\\""'
-<<<<<<< HEAD
       WHERE value = '"false"';
-=======
-      WHERE value = 'false'::jsonb;
->>>>>>> 5aa47978734b4deae5139a4fd5eba15fd20cb874
     `)
   }
 }
