b9e512e98f23cbc01bde80ccc8628d888b438a85
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2025-01-23 13:56:34 +0100

    exclude all deps as external

--- packages/admin/admin-bundler/src/lib/plugin.ts
+++ packages/admin/admin-bundler/src/lib/plugin.ts
@@ -62,7 +62,11 @@ export async function plugin(options: PluginOptions) {
       minify: false,
       outDir: path.resolve(options.root, options.outDir),
       rollupOptions: {
-        external: [...external],
+        external: [
+          ...external,
+          /^node_modules/,
+          /^@?[A-Za-z].*/, // This will treat all packages starting with @ or letters as external
+        ],
         output: {
           globals: {
             react: "React",
