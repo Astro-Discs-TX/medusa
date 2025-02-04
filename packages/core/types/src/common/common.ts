efcdf5fba875f0c76254eb93c7deb1f6ca251765
Ismail Sabet <47750560+ismailsabet@users.noreply.github.com>
2025-02-04 19:52:48 +0200

    Update common.ts
    Not sure if API references are auto-generated from this file, but this typo is on the store and admin API pages.

--- packages/core/types/src/common/common.ts
+++ packages/core/types/src/common/common.ts
@@ -128,8 +128,8 @@ export type TotalField =
 export type RequestQueryFields = {
   /**
    * Comma-separated fields that should be included in the returned data.
-   * if a field is prefixed with `+` it will be added to the default fields, using `-` will remove it from the default fields.
-   * without prefix it will replace the entire default fields.
+   * If a field is prefixed with `+` it will be added to the default fields, using `-` will remove it from the default fields.
+   * Without prefix it will replace the entire default fields.
    */
   fields?: string
 
