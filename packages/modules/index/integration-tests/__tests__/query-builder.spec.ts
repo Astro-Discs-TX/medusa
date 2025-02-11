109772ccfaf878335152177adadf063bc85c9527
Carlos R. L. Rodrigues <rodrigolr@gmail.com>
2025-02-11 13:48:58 -0300

    revert

--- packages/modules/index/integration-tests/__tests__/query-builder.spec.ts
+++ packages/modules/index/integration-tests/__tests__/query-builder.spec.ts
@@ -508,6 +508,86 @@ describe("IndexModuleService query", function () {
     ])
   })
 
+  it("should query products filtering by variant sku", async () => {
+    const { data, metadata } = await module.query({
+      fields: ["product.*", "product.variants.*", "product.variants.prices.*"],
+      filters: {
+        product: {
+          variants: {
+            sku: { $like: "aaa%" },
+          },
+        },
+      },
+      pagination: {
+        take: 100,
+        skip: 0,
+      },
+    })
+
+    expect(metadata).toEqual({
+      count: 1,
+      skip: 0,
+      take: 100,
+    })
+
+    expect(data).toEqual([
+      {
+        id: "prod_1",
+        variants: [
+          {
+            id: "var_1",
+            sku: "aaa test aaa",
+            prices: [
+              {
+                id: "money_amount_1",
+                amount: 100,
+              },
+            ],
+          },
+        ],
+      },
+    ])
+  })
+
+  it("should query products filtering by variant sku and join filters on prices amount", async () => {
+    const { data, metadata } = await module.query({
+      fields: ["product.*", "product.variants.*", "product.variants.prices.*"],
+      joinFilters: {
+        "product.variants.prices.amount": { $gt: 110 },
+      },
+      filters: {
+        product: {
+          variants: {
+            sku: { $like: "aaa%" },
+          },
+        },
+      },
+      pagination: {
+        take: 100,
+        skip: 0,
+      },
+    })
+
+    expect(metadata).toEqual({
+      count: 1,
+      skip: 0,
+      take: 100,
+    })
+
+    expect(data).toEqual([
+      {
+        id: "prod_1",
+        variants: [
+          {
+            id: "var_1",
+            sku: "aaa test aaa",
+            prices: [],
+          },
+        ],
+      },
+    ])
+  })
+
   it("should filter using fields not selected", async () => {
     const { data } = await module.query({
       fields: ["product.id", "product.variants.*"],
@@ -545,68 +625,8 @@ describe("IndexModuleService query", function () {
     ])
   })
 
-  it("should query products filtering by variant sku", async () => {
-    const { data } = await module.query({
-      fields: ["product.*", "product.variants.*", "product.variants.prices.*"],
-      filters: {
-        product: {
-          variants: {
-            sku: { $like: "aaa%" },
-          },
-        },
-      },
-    })
-
-    expect(data).toEqual([
-      {
-        id: "prod_1",
-        variants: [
-          {
-            id: "var_1",
-            sku: "aaa test aaa",
-            prices: [
-              {
-                id: "money_amount_1",
-                amount: 100,
-              },
-            ],
-          },
-        ],
-      },
-    ])
-  })
-
-  it("should query products filtering by variant sku", async () => {
-    const { data } = await module.query({
-      fields: ["product.*", "product.variants.*", "product.variants.prices.*"],
-      joinFilters: {
-        "product.variants.prices.amount": { $gt: 110 },
-      },
-      filters: {
-        product: {
-          variants: {
-            sku: { $like: "aaa%" },
-          },
-        },
-      },
-    })
-
-    expect(data).toEqual([
-      {
-        id: "prod_1",
-        variants: [
-          {
-            id: "var_1",
-            sku: "aaa test aaa",
-            prices: [],
-          },
-        ],
-      },
-    ])
-  })
-
   it("should query products filtering by price and returning the complete entity", async () => {
-    const { data } = await module.query({
+    const { data, metadata } = await module.query({
       fields: ["product.*", "product.variants.*", "product.variants.prices.*"],
       filters: {
         product: {
@@ -618,6 +638,16 @@ describe("IndexModuleService query", function () {
         },
       },
       keepFilteredEntities: true,
+      pagination: {
+        take: 100,
+        skip: 0,
+      },
+    })
+
+    expect(metadata).toEqual({
+      count: 1,
+      skip: 0,
+      take: 100,
     })
 
     expect(data).toEqual([
@@ -724,7 +754,7 @@ describe("IndexModuleService query", function () {
   })
 
   it("should handle null values on where clause", async () => {
-    const { data } = await module.query({
+    const { data: data_, metadata } = await module.query({
       fields: ["product.*", "product.variants.*", "product.variants.prices.*"],
       filters: {
         product: {
@@ -733,19 +763,63 @@ describe("IndexModuleService query", function () {
           },
         },
       },
+      pagination: {
+        take: 100,
+        skip: 0,
+      },
+    })
+
+    expect(metadata).toEqual({
+      count: 1,
+      skip: 0,
+      take: 100,
+    })
+
+    expect(data_).toEqual([
+      {
+        id: "prod_2",
+        deep: { a: 1, obj: { b: 15 } },
+        title: "Product 2 title",
+        variants: [],
+      },
+    ])
+
+    const { data, metadata: metadata2 } = await module.query({
+      fields: ["product.*", "product.variants.*", "product.variants.prices.*"],
+      filters: {
+        product: {
+          variants: {
+            sku: { $ne: null },
+          },
+        },
+      },
+      pagination: {
+        take: 100,
+        skip: 0,
+      },
+    })
+
+    expect(metadata2).toEqual({
+      count: 1,
+      skip: 0,
+      take: 100,
     })
 
     expect(data).toEqual([
       {
-        id: "prod_2",
-        title: "Product 2 title",
-        deep: {
-          a: 1,
-          obj: {
-            b: 15,
+        id: "prod_1",
+        variants: [
+          {
+            id: "var_1",
+            sku: "aaa test aaa",
+            prices: [{ id: "money_amount_1", amount: 100 }],
           },
-        },
-        variants: [],
+          {
+            id: "var_2",
+            sku: "sku 123",
+            prices: [{ id: "money_amount_2", amount: 10 }],
+          },
+        ],
       },
     ])
   })
