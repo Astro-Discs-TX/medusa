ac063f1f9e6cf4f480bf9de5bd97eb126e58a60f
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2025-02-18 10:55:48 +0100

    fix(core-flows): Allow adding shipping methods through order edits

--- integration-tests/http/__tests__/order-edits/order-edits.spec.ts
+++ integration-tests/http/__tests__/order-edits/order-edits.spec.ts
@@ -516,16 +516,18 @@ medusaIntegrationTestRunner({
 
         const orderId = order.id
 
-        const shippingMethod = await api.post(
+        const shippingMethodResponse = await api.post(
           `/admin/order-edits/${orderId}/shipping-method`,
           { shipping_option_id: shippingOption.id, custom_amount: 5 },
           adminHeaders
         )
 
         expect(
-          shippingMethod.data.order_preview.shipping_methods.length
+          shippingMethodResponse.data.order_preview.shipping_methods.length
         ).toEqual(2)
-        expect(shippingMethod.data.order_preview.shipping_methods).toEqual(
+        expect(
+          shippingMethodResponse.data.order_preview.shipping_methods
+        ).toEqual(
           expect.arrayContaining([
             expect.objectContaining({
               amount: 10,
@@ -534,6 +536,45 @@ medusaIntegrationTestRunner({
               amount: 5,
             }),
           ])
+        )
+
+        const requestResult = await api.post(
+          `/admin/order-edits/${orderId}/request`,
+          {},
+          adminHeaders
+        )
+
+        expect(requestResult.data.order_preview.order_change.status).toEqual(
+          OrderChangeStatus.REQUESTED
+        )
+
+        await api.post(
+          `/admin/order-edits/${orderId}/confirm`,
+          {},
+          adminHeaders
+        )
+
+        const orderResult = await api.get(
+          `/admin/orders/${orderId}`,
+          adminHeaders
+        )
+
+        expect(orderResult.data.order.shipping_methods.length).toEqual(2)
+        expect(orderResult.data.order.shipping_methods).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ amount: 10 }),
+            expect.objectContaining({ amount: 5 }),
+          ])
+        )
+
+        const orderChangesResult = await api.get(
+          `/admin/orders/${orderId}/changes?change_type=edit`,
+          adminHeaders
+        )
+
+        expect(orderChangesResult.data.order_changes.length).toEqual(1)
+        expect(orderChangesResult.data.order_changes[0].status).toEqual(
+          OrderChangeStatus.CONFIRMED
         )
       })
     })
