52da217fc5ab502e5e40343354d4febc78a72738
Harminder Virk <virk.officials@gmail.com>
2025-03-11 16:18:33 +0530

    refactor: use module name as the snapshot name

--- packages/core/utils/src/modules-sdk/mikro-orm-cli-config-builder.ts
+++ packages/core/utils/src/modules-sdk/mikro-orm-cli-config-builder.ts
@@ -61,6 +61,7 @@ export function defineMikroOrmCliConfig(
     ...(options as any),
     entities: entities.filter(Boolean),
     migrations: {
+      snapshotName: databaseName,
       generator: CustomTsMigrationGenerator,
       ...options.migrations,
     },
