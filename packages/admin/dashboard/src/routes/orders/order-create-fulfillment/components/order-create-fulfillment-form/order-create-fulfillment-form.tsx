c8d8504b851d8a382cbe2f5446a23e0ff9b79c3a
fPolic <mainacc.polic@gmail.com>
2025-03-27 11:02:47 +0100

    chore: refactor, changesets

--- packages/admin/dashboard/src/routes/orders/order-create-fulfillment/components/order-create-fulfillment-form/order-create-fulfillment-form.tsx
+++ packages/admin/dashboard/src/routes/orders/order-create-fulfillment/components/order-create-fulfillment-form/order-create-fulfillment-form.tsx
@@ -148,8 +148,6 @@ export function OrderCreateFulfillmentForm({
       toast.success(t("orders.fulfillment.toast.created"))
       handleSuccess(`/orders/${order.id}`)
     } catch (e) {
-      console.log(e)
-
       toast.error(e.message)
     }
   })
