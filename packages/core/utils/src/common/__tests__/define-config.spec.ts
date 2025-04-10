e2763e7c8526e413393c0ade9902cb634ed5c4e7
Harminder Virk <virk.officials@gmail.com>
2025-04-10 17:44:24 +0530

    test: add test for manually specifying dynamodb options

--- packages/core/utils/src/common/__tests__/define-config.spec.ts
+++ packages/core/utils/src/common/__tests__/define-config.spec.ts
@@ -1163,6 +1163,7 @@ describe("defineConfig", function () {
     process.env.S3_BUCKET = "medusa-cloud-test"
     process.env.S3_ENDPOINT = "https://s3.amazonaws.com"
     process.env.SESSION_STORE = "dynamodb"
+    process.env.DYNAMO_DB_ENDPOINT = "http://localhost:8000"
     const res = defineConfig({})
 
     process.env = { ...originalEnv }
@@ -1324,6 +1325,9 @@ describe("defineConfig", function () {
         "projectConfig": {
           "databaseUrl": "postgres://localhost/medusa-starter-default",
           "dynamodbOptions": {
+            "clientOptions": {
+              "endpoint": "http://localhost:8000",
+            },
             "readCapacityUnits": 5,
             "skipThrowMissingSpecialKeys": true,
             "table": "medusa-sessions",
@@ -1347,6 +1351,178 @@ describe("defineConfig", function () {
             "retryStrategy": [Function],
           },
           "redisUrl": "redis://localhost:6379",
+        },
+      }
+    `)
+  })
+
+  it("should allow custom dynamodb config", function () {
+    expect(
+      defineConfig({
+        projectConfig: {
+          http: {
+            adminCors: "http://localhost:3000",
+          } as any,
+          dynamodbOptions: {
+            clientOptions: {
+              endpoint: "http://localhost:8000",
+            },
+            table: "medusa-sessions",
+            writeCapacityUnits: 25,
+            readCapacityUnits: 25,
+          },
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "admin": {
+          "backendUrl": "/",
+          "path": "/app",
+        },
+        "featureFlags": {},
+        "modules": {
+          "api_key": {
+            "resolve": "@medusajs/medusa/api-key",
+          },
+          "auth": {
+            "options": {
+              "providers": [
+                {
+                  "id": "emailpass",
+                  "resolve": "@medusajs/medusa/auth-emailpass",
+                },
+              ],
+            },
+            "resolve": "@medusajs/medusa/auth",
+          },
+          "cache": {
+            "resolve": "@medusajs/medusa/cache-inmemory",
+          },
+          "cart": {
+            "resolve": "@medusajs/medusa/cart",
+          },
+          "currency": {
+            "resolve": "@medusajs/medusa/currency",
+          },
+          "customer": {
+            "resolve": "@medusajs/medusa/customer",
+          },
+          "event_bus": {
+            "resolve": "@medusajs/medusa/event-bus-local",
+          },
+          "file": {
+            "options": {
+              "providers": [
+                {
+                  "id": "local",
+                  "resolve": "@medusajs/medusa/file-local",
+                },
+              ],
+            },
+            "resolve": "@medusajs/medusa/file",
+          },
+          "fulfillment": {
+            "options": {
+              "providers": [
+                {
+                  "id": "manual",
+                  "resolve": "@medusajs/medusa/fulfillment-manual",
+                },
+              ],
+            },
+            "resolve": "@medusajs/medusa/fulfillment",
+          },
+          "inventory": {
+            "resolve": "@medusajs/medusa/inventory",
+          },
+          "locking": {
+            "resolve": "@medusajs/medusa/locking",
+          },
+          "notification": {
+            "options": {
+              "providers": [
+                {
+                  "id": "local",
+                  "options": {
+                    "channels": [
+                      "feed",
+                    ],
+                    "name": "Local Notification Provider",
+                  },
+                  "resolve": "@medusajs/medusa/notification-local",
+                },
+              ],
+            },
+            "resolve": "@medusajs/medusa/notification",
+          },
+          "order": {
+            "resolve": "@medusajs/medusa/order",
+          },
+          "payment": {
+            "resolve": "@medusajs/medusa/payment",
+          },
+          "pricing": {
+            "resolve": "@medusajs/medusa/pricing",
+          },
+          "product": {
+            "resolve": "@medusajs/medusa/product",
+          },
+          "promotion": {
+            "resolve": "@medusajs/medusa/promotion",
+          },
+          "region": {
+            "resolve": "@medusajs/medusa/region",
+          },
+          "sales_channel": {
+            "resolve": "@medusajs/medusa/sales-channel",
+          },
+          "stock_location": {
+            "resolve": "@medusajs/medusa/stock-location",
+          },
+          "store": {
+            "resolve": "@medusajs/medusa/store",
+          },
+          "tax": {
+            "resolve": "@medusajs/medusa/tax",
+          },
+          "user": {
+            "options": {
+              "jwt_secret": "supersecret",
+            },
+            "resolve": "@medusajs/medusa/user",
+          },
+          "workflows": {
+            "resolve": "@medusajs/medusa/workflow-engine-inmemory",
+          },
+        },
+        "plugins": [],
+        "projectConfig": {
+          "databaseUrl": "postgres://localhost/medusa-starter-default",
+          "dynamodbOptions": {
+            "clientOptions": {
+              "endpoint": "http://localhost:8000",
+            },
+            "readCapacityUnits": 25,
+            "table": "medusa-sessions",
+            "writeCapacityUnits": 25,
+          },
+          "http": {
+            "adminCors": "http://localhost:3000",
+            "authCors": "http://localhost:7000,http://localhost:7001,http://localhost:5173",
+            "cookieSecret": "supersecret",
+            "jwtSecret": "supersecret",
+            "restrictedFields": {
+              "store": [
+                ${DEFAULT_STORE_RESTRICTED_FIELDS.map((v) => `"${v}"`).join(
+                  ",\n                "
+                )},
+              ],
+            },
+            "storeCors": "http://localhost:8000",
+          },
+          "redisOptions": {
+            "retryStrategy": [Function],
+          },
         },
       }
     `)
