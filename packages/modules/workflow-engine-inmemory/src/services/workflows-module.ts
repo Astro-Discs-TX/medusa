7da9a7a2b6d923d5f134582064d64c6327d3eba1
adrien2p <adrien.deperetti@gmail.com>
2025-04-15 17:56:22 +0200

    fix(): Properly handle workflow as step now that events are fixed entirely

--- packages/modules/workflow-engine-inmemory/src/services/workflows-module.ts
+++ packages/modules/workflow-engine-inmemory/src/services/workflows-module.ts
@@ -237,7 +237,7 @@ export class WorkflowsModuleService<
     options: WorkflowOrchestratorCancelOptions,
     @MedusaContext() context: Context = {}
   ) {
-    return this.workflowOrchestratorService_.cancel(
+    return await this.workflowOrchestratorService_.cancel(
       workflowIdOrWorkflow,
       options
     )
