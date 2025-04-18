9710a6d7e75f51a8f9ee7b1c79518118b6dd04b9
Shahed Nasser <shahednasser@gmail.com>
2025-04-18 17:30:25 +0300

    test

--- www/apps/resources/next.config.mjs
+++ www/apps/resources/next.config.mjs
@@ -179,8 +179,8 @@ const nextConfig = {
     "*": ["node_modules/@medusajs/icons"],
   },
   outputFileTracingIncludes: {
-    "/md-content/\\[\\[\\.\\.\\.slug\\]\\]": ["./app/**/*"],
-    "/md-content/references/**": ["./references/**/*"],
+    "/md\\-content/\\[\\[\\.\\.\\.slug\\]\\]": ["./app/**/*"],
+    "/md\\-content/references/**": ["./references/**/*"],
   },
   experimental: {
     optimizePackageImports: ["@medusajs/icons", "@medusajs/ui", "elkjs"],
