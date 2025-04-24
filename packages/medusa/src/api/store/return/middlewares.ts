4feea05b1f36cff96ddc28c2b238d237fe678809
Riqwan Thamir <rmthamir@gmail.com>
2025-04-24 12:12:25 +0200

    fix(medusa): Fix store return API

--- packages/medusa/src/api/store/return/middlewares.ts
+++ packages/medusa/src/api/store/return/middlewares.ts
@@ -1,15 +1,15 @@
-import { MiddlewareRoute } from "@medusajs/framework/http"
 import {
   validateAndTransformBody,
   validateAndTransformQuery,
 } from "@medusajs/framework"
+import { MiddlewareRoute } from "@medusajs/framework/http"
 import * as QueryConfig from "./query-config"
 import { ReturnsParams, StorePostReturnsReqSchema } from "./validators"
 
 export const storeReturnRoutesMiddlewares: MiddlewareRoute[] = [
   {
     method: ["POST"],
-    matcher: "/store/returns",
+    matcher: "/store/return",
     middlewares: [
       validateAndTransformBody(StorePostReturnsReqSchema),
       validateAndTransformQuery(
