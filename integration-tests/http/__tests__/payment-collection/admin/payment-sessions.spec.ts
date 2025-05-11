aae09ec9d97315fc13cbb334ceedb23e205955b9
olivermrbl <oliver@mrbltech.com>
2025-05-11 18:29:12 +0200

    chore: Fix tests

--- integration-tests/http/__tests__/payment-collection/admin/payment-sessions.spec.ts
+++ integration-tests/http/__tests__/payment-collection/admin/payment-sessions.spec.ts
@@ -38,6 +38,7 @@ medusaIntegrationTestRunner({
       let product
       let cart
       let shippingProfile
+      let salesChannel
 
       beforeEach(async () => {
         region = (
@@ -51,6 +52,14 @@ medusaIntegrationTestRunner({
             adminHeaders
           )
         ).data.region
+
+        salesChannel = (
+          await api.post(
+            "/admin/sales-channels",
+            { name: "Web" },
+            adminHeaders
+          )
+        ).data.sales_channel
 
         shippingProfile = (
           await api.post(
@@ -86,100 +95,94 @@ medusaIntegrationTestRunner({
         ).data.product
       })
 
-      describe("when the customer is guest", () => {
-        beforeEach(async () => {
-          cart = (
-            await api.post(
-              "/store/carts",
-              {
-                region_id: region.id,
-                items: [{ variant_id: product.variants[0].id, quantity: 1 }],
-              },
-              storeHeaders
-            )
-          ).data.cart
-        })
-
-        it("should create a payment session", async () => {
-          const paymentCollection = (
-            await api.post(
-              `/store/payment-collections`,
-              {
-                cart_id: cart.id,
-              },
-              storeHeaders
-            )
-          ).data.payment_collection
-
+      it("should create a session when the customer is a guest", async () => {
+        cart = (
           await api.post(
-            `/store/payment-collections/${paymentCollection.id}/payment-sessions`,
-            { provider_id: "pp_system_default" },
+            "/store/carts",
+            {
+              region_id: region.id,
+              items: [{ variant_id: product.variants[0].id, quantity: 1 }],
+              sales_channel_id: salesChannel.id,
+            },
             storeHeaders
           )
+        ).data.cart
 
-          // Adding a second payment session to ensure only one session gets created
-          const {
-            data: { payment_collection },
-          } = await api.post(
-            `/store/payment-collections/${paymentCollection.id}/payment-sessions`,
-            { provider_id: "pp_system_default" },
+        const paymentCollection = (
+          await api.post(
+            `/store/payment-collections`,
+            {
+              cart_id: cart.id,
+            },
             storeHeaders
           )
+        ).data.payment_collection
 
-          expect(payment_collection.payment_sessions).toEqual([
-            expect.objectContaining({
-              currency_code: "usd",
-              provider_id: "pp_system_default",
-              status: "pending",
-              amount: 150,
-            }),
-          ])
-        })
+        await api.post(
+          `/store/payment-collections/${paymentCollection.id}/payment-sessions`,
+          { provider_id: "pp_system_default" },
+          storeHeaders
+        )
+
+        // Adding a second payment session to ensure only one session gets created
+        const {
+          data: { payment_collection },
+        } = await api.post(
+          `/store/payment-collections/${paymentCollection.id}/payment-sessions`,
+          { provider_id: "pp_system_default" },
+          storeHeaders
+        )
+
+        expect(payment_collection.payment_sessions).toEqual([
+          expect.objectContaining({
+            currency_code: "usd",
+            provider_id: "pp_system_default",
+            status: "pending",
+            amount: 150,
+          }),
+        ])
       })
 
-      describe("when the customer is authenticated", () => {
-        beforeEach(async () => {
-          cart = (
-            await api.post(
-              "/store/carts",
-              {
-                region_id: region.id,
-                items: [{ variant_id: product.variants[0].id, quantity: 1 }],
-              },
-              storeHeadersWithCustomer
-            )
-          ).data.cart
-        })
-
-        it("should create a payment session", async () => {
-          const paymentCollection = (
-            await api.post(
-              `/store/payment-collections`,
-              { cart_id: cart.id },
-              storeHeadersWithCustomer
-            )
-          ).data.payment_collection
-
-          const {
-            data: { payment_collection },
-          } = await api.post(
-            `/store/payment-collections/${paymentCollection.id}/payment-sessions`,
-            { provider_id: "pp_system_default" },
+      it("should create a session when the customer is authenticated", async () => {
+        cart = (
+          await api.post(
+            "/store/carts",
+            {
+              region_id: region.id,
+              items: [{ variant_id: product.variants[0].id, quantity: 1 }],
+              sales_channel_id: salesChannel.id,
+            },
             storeHeadersWithCustomer
           )
+        ).data.cart
 
-          // TODO: This does not create an account holder as the system payment provider does not support it
-          // Create a custom system payment provider that supports it or add account holder support to the system payment provider
-          // This test will pass through the account holder creation step though, which is what the test is checking for
-          expect(payment_collection.payment_sessions).toEqual([
-            expect.objectContaining({
-              currency_code: "usd",
-              provider_id: "pp_system_default",
-              status: "pending",
-              amount: 150,
-            }),
-          ])
-        })
+        const paymentCollection = (
+          await api.post(
+            `/store/payment-collections`,
+            { cart_id: cart.id },
+            storeHeadersWithCustomer
+          )
+        ).data.payment_collection
+
+        const {
+          data: { payment_collection },
+        } = await api.post(
+          `/store/payment-collections/${paymentCollection.id}/payment-sessions`,
+          { provider_id: "pp_system_default" },
+          storeHeadersWithCustomer
+        )
+
+        // TODO: This does not create an account holder as the system payment provider does not support it
+        // Create a custom system payment provider that supports it or add account holder support to the system payment provider
+        // This test will pass through the account holder creation step though, which is what the test is checking for
+        expect(payment_collection.payment_sessions).toEqual([
+          expect.objectContaining({
+            currency_code: "usd",
+            provider_id: "pp_system_default",
+            status: "pending",
+            amount: 150,
+          }),
+        ])
       })
     })
   },
