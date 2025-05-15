5f63d317ea6c4f2d0df6cd51f1c99aa99b8998c7
olivermrbl <olivermrbl@users.noreply.github.com>
2025-05-15 08:14:29 +0000

    chore(docs): Generated API Reference (automated)

---
+++ www/utils/generated/oas-output/operations/admin/post_admin_uploads_presigned-urls.ts
@@ -0,0 +1,52 @@
+/**
+ * @oas [post] /admin/uploads/presigned-urls
+ * operationId: PostUploadsPresignedUrls
+ * summary: Create Upload
+ * description: Create a upload.
+ * x-authenticated: true
+ * parameters: []
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ *   - jwt_token: []
+ * requestBody:
+ *   content:
+ *     application/json:
+ *       schema:
+ *         $ref: "#/components/schemas/AdminUploadPreSignedUrl"
+ * x-codeSamples:
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |-
+ *       curl -X POST '{backend_url}/admin/uploads/presigned-urls' \
+ *       -H 'Authorization: Bearer {access_token}' \
+ *       -H 'Content-Type: application/json' \
+ *       --data-raw '{
+ *         "originalname": "{value}",
+ *         "size": 15,
+ *         "mime_type": "{value}"
+ *       }'
+ * tags:
+ *   - Uploads
+ * responses:
+ *   "200":
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/AdminUploadPreSignedUrlResponse"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "401":
+ *     $ref: "#/components/responses/unauthorized"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ * 
+*/
+
