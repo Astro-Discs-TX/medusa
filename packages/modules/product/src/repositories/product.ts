338fa40975075eb44d86f9f9a62a024bbe759aad
adrien2p <adrien.deperetti@gmail.com>
2025-05-20 09:59:33 +0200

    fix(product): Deep update data retrieval bottleneck

--- packages/modules/product/src/repositories/product.ts
+++ packages/modules/product/src/repositories/product.ts
@@ -1,10 +1,14 @@
 import { Product, ProductOption } from "@models"
 
 import { Context, DAL, InferEntityType } from "@medusajs/framework/types"
-import { buildQuery, DALUtils } from "@medusajs/framework/utils"
+import {
+  arrayDifference,
+  buildQuery,
+  DALUtils,
+  MedusaError,
+} from "@medusajs/framework/utils"
 import { SqlEntityManager, wrap } from "@mikro-orm/postgresql"
 
-// eslint-disable-next-line max-len
 export class ProductRepository extends DALUtils.mikroOrmBaseRepositoryFactory(
   Product
 ) {
@@ -13,28 +17,78 @@ export class ProductRepository extends DALUtils.mikroOrmBaseRepositoryFactory(
     super(...arguments)
   }
 
+  /**
+   * Identify the relations to load for the given update.
+   * @param update
+   * @returns
+   */
+  getRelationsToLoad(update: any[]): string[] {
+    const relationsToLoad = new Set<string>()
+    update.forEach((update) => {
+      if (update.options) {
+        relationsToLoad.add("options")
+        relationsToLoad.add("options.values")
+      }
+      if (update.variants) {
+        relationsToLoad.add("options")
+        relationsToLoad.add("options.values")
+        relationsToLoad.add("variants")
+        relationsToLoad.add("variants.options")
+        relationsToLoad.add("variants.options.option")
+      }
+      if (update.tags) relationsToLoad.add("tags")
+      if (update.categories) relationsToLoad.add("categories")
+      if (update.images) relationsToLoad.add("images")
+      if (update.collection) relationsToLoad.add("collection")
+      if (update.type) relationsToLoad.add("type")
+    })
+    return Array.from(relationsToLoad)
+  }
+
   async deepUpdate(
-    updates: any[],
+    updates: ({ id: string } & any)[],
     validateVariantOptions: (
       variants: any[],
       options: InferEntityType<typeof ProductOption>[]
     ) => void,
     context: Context = {}
   ): Promise<InferEntityType<typeof Product>[]> {
-    updates.forEach((update) => this.correctUpdateDTOTypes(update))
+    const productIdsToUpdate: string[] = []
+    updates.forEach((update) => {
+      this.correctUpdateDTOTypes(update)
+      productIdsToUpdate.push(update.id)
+    })
 
-    const products = await this.find(
-      buildQuery({ id: updates.map((p) => p.id) }, { relations: ["*"] }),
-      context
+    const relationsToLoad = this.getRelationsToLoad(updates)
+
+    const findOptions = buildQuery(
+      { id: productIdsToUpdate },
+      {
+        relations: relationsToLoad,
+        take: updates.length,
+      }
     )
+
+    const products = await this.find(findOptions, context)
     const productsMap = new Map(products.map((p) => [p.id, p]))
+
+    const productIds = Array.from(productsMap.keys())
+    const productsNotFound = arrayDifference(productIdsToUpdate, productIds)
+
+    if (productsNotFound.length > 0) {
+      throw new MedusaError(
+        MedusaError.Types.NOT_FOUND,
+        `Unable to update the products with ids: ${productsNotFound.join(", ")}`
+      )
+    }
 
     for (const update of updates) {
       const product = productsMap.get(update.id)!
+      const wrappedProduct = wrap(product)
 
       // Assign the options first, so they'll be available for the variants loop below
       if (update.options) {
-        wrap(product).assign({ options: update.options })
+        wrappedProduct.assign({ options: update.options })
         delete update.options // already assigned above, so no longer necessary
       }
 
@@ -71,15 +125,13 @@ export class ProductRepository extends DALUtils.mikroOrmBaseRepositoryFactory(
         }))
       }
 
-      wrap(product!).assign(update)
+      wrappedProduct.assign(update)
     }
 
     // Doing this to ensure updates are returned in the same order they were provided,
     // since some core flows rely on this.
     // This is a high level of coupling though.
-    return updates
-      .map((update) => productsMap.get(update.id))
-      .filter((product) => product !== undefined)
+    return updates.map((update) => productsMap.get(update.id)!)
   }
 
   // We should probably fix the column types in the database to avoid this
