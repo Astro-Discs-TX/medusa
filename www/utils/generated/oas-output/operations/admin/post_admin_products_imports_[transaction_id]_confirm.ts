3c815df7fa81dfcc67dd7db4edc5ceb0c5fff5a9
olivermrbl <olivermrbl@users.noreply.github.com>
2025-05-22 12:31:19 +0000

    chore(docs): Generated API Reference (automated)

---
+++ www/utils/generated/oas-output/operations/admin/post_admin_products_imports_[transaction_id]_confirm.ts
@@ -0,0 +1,44 @@
+/**
+ * @oas [post] /admin/products/imports/{transaction_id}/confirm
+ * operationId: PostProductsImportsTransaction_idConfirm
+ * summary: Add Confirm to Product
+ * description: Add a Confirm to a product
+ * x-authenticated: true
+ * parameters:
+ *   - name: transaction_id
+ *     in: path
+ *     description: The product's transaction id.
+ *     required: true
+ *     schema:
+ *       type: string
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ *   - jwt_token: []
+ * x-codeSamples:
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |-
+ *       curl -X POST '{backend_url}/admin/products/imports/{transaction_id}/confirm' \
+ *       -H 'Authorization: Bearer {access_token}'
+ * tags:
+ *   - Products
+ * responses:
+ *   "202":
+ *     description: OK
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "401":
+ *     $ref: "#/components/responses/unauthorized"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ * x-version: 2.8.0
+ * 
+*/
+
