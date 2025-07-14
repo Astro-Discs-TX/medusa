58469660e6dd8b9f4a533f159159310923e6de55
Shahed Nasser <shahednasser@gmail.com>
2025-07-14 14:45:26 +0300

    updated faq

--- www/apps/cloud/generated/edit-dates.mjs
+++ www/apps/cloud/generated/edit-dates.mjs
@@ -18,5 +18,5 @@ export const generatedEditDates = {
   "app/environments/environment-variables/page.mdx": "2025-06-25T07:42:53.521Z",
   "app/environments/long-lived/page.mdx": "2025-07-14T11:01:54.536Z",
   "app/environments/preview/page.mdx": "2025-06-25T08:00:00.839Z",
-  "app/faq/page.mdx": "2025-06-25T08:00:26.853Z"
+  "app/faq/page.mdx": "2025-07-14T11:44:58.587Z"
 }
